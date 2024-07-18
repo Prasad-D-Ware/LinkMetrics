d2b4c0b8587c762bbc0d96e8b491c9b9f4f7f40c
Prasad-D-Ware <prasadware42116@gmail.com>
2024-07-18 17:41:18 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from 'react-router-dom'
+import './App.css'
+import { Children } from 'react'
+
+
+
+function App() {
+
+  return (
+    <div className='text-5xl'>
+     Welcome to LinkMetrics
+    </div>
+  )
+}
+
+export default App
