34127ef60ed59b289e571da39e141a1f94e0ccdc
Prasad-D-Ware <prasadware42116@gmail.com>
2024-07-19 23:41:52 +0530

    added components

---
+++ src/components/Header.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { LinkIcon, LogOut } from "lucide-react";
+
+const Header = () => {
+  const navigate = useNavigate();
+  const user = true;
+  return (
+    <nav className="py-4 flex justify-between items-center">
+      <Link>
+        <div>
+          <img src="/logo.png" alt="LinkMetrics Logo" className="h-6" />
+        </div>
+      </Link>
+
+      <div>
+        {!user ? (
+          <Button
+            onClick={() => {
+              navigate("/auth");
+            }}
+          >
+            Login
+          </Button>
+        ) : (
+          <DropdownMenu>
+            <DropdownMenuTrigger className="w-10 overflow-hidden rounded-full">
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>PW</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Prasad Ware</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <span>
+                  <LinkIcon className="mr-2 h-4 w-4" />
+                </span>
+                My Links
+              </DropdownMenuItem>
+              <DropdownMenuItem className="text-red-400">
+                <span>
+                  <LogOut className="h-4 w-4 mr-2" />
+                </span>
+                Logout
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
