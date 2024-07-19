77e36738c0971afb01bdf95ff773956e71b7accb
Prasad-D-Ware <prasadware42116@gmail.com>
2024-07-19 23:42:45 +0530

    added routing and applayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
-import { createBrowserRouter } from 'react-router-dom'
-import './App.css'
-import { Children } from 'react'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import AppLayout from "./layouts/app-layout";
+import Landing from "./pages/landing";
+import Dashboard from "./pages/dashboard";
+import Auth from "./pages/auth";
+import Link from "./pages/link";
+import RedirectLink from "./pages/redirect-link";
 
-
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Landing />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/auth",
+        element: <Auth />,
+      },
+      {
+        path: "/link/:id",
+        element: <Link />,
+      },
+      {
+        path: "/:id",
+        element: <RedirectLink />,
+      },
+    ],
+  },
+]);
 
 function App() {
-
-  return (
-    <div className='text-5xl'>
-     Welcome to LinkMetrics
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from '../components/Header'
+
+const AppLayout = () => {
+  return (
+    <div>
+      <main className='min-h-screen container'>
+      <Header />
+    <Outlet/>
+      </main>
+      <div className='p-10 text-center bg-gray-800 mt-10'>
+        Made with ❤️ by Prasad Ware
+      </div>
+    </div>
+  )
+}
+
+export default AppLayout
