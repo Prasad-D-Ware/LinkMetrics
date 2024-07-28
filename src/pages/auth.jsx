08baeb79641b9a11dfec3d59884fa8f255ed916b
Prasad-D-Ware <prasadware42116@gmail.com>
2024-07-28 23:51:09 +0530

    feat:securing routes

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,15 +1,28 @@
-import React from "react";
-import { useSearchParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Login from "../components/Login";
 import Signup from "../components/Signup";
+import { UrlState } from "../context";
 
 const Auth = () => {
   const [searchParams] = useSearchParams();
+  const longLink = searchParams.get("createNew")
+  const navigate = useNavigate();
+
+  const { isAuthenticated, loading} = UrlState();
+
+  useEffect(()=>{
+    if(isAuthenticated && !loading){
+      navigate(`/dashboard?${longLink ? `createNew=${longLink}` : ""}`);
+    }
+  },[isAuthenticated,loading])
+
+  
   return (
-    <div className="mt-36 flex flex-col items-center gap-10">
+    <div className="mt-24 flex flex-col items-center gap-10">
       <h1 className="text-5xl font-extrabold">
-        {searchParams.get("createNew")
+        {longLink
           ? "Hold up! Let's Login first..."
           : "Login / SignUp"}
       </h1>
